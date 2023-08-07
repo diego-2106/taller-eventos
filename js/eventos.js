3f06e23df82ca4f51341ac806077b2e08d0cf03f
diego-2106 <ez.mm.gg@gmail.com>
2023-08-07 12:19:05 -0300

    Solucionado el problema del click

--- js/eventos.js
+++ js/eventos.js
@@ -1,5 +1,6 @@
 const div = document.querySelector('.contenedor');
 
-div.addEventListener('click', () => {
+div.addEventListener('click', e => {
+    if(e.target !== document.querySelector('#button'))
     alert('Hola! Soy el div');
 })
